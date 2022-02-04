a189802280d70a1676568a1ac8b2a5eca507001a
Ice-mourne <Cboweris@gmail.com>
2022-02-04 14:22:55 +0200

    updated types

--- src/ts/convertDescription.ts
+++ src/ts/convertDescription.ts
@@ -1,16 +1,44 @@
-type splittedDescription = {
-   lineText?: string
-   className?: string
-   table?: rowsLines[]
-}
 type rowsLines = {
    lineText: string
    className?: string
 }
-
 type convertedLines = {
    lineText: string[] | null
    className?: string
+}
+namespace Complete {
+   export interface Description {
+      lineText?: Array<LineText>
+      className?: string
+      table?: Table[]
+   }
+
+   export interface Table {
+      lineText?: Array<LineText[]>
+      className?: string
+   }
+
+   export interface LineText {
+      text?: string
+      className?: string
+      formula?: string
+      formulaText?: string
+      linkText?: string
+      linkUrl?: string
+      title?: string
+   }
+}
+namespace LineTextString {
+   export interface Description {
+      lineText?: string[] | string
+      className?: string
+      table?: Table[]
+   }
+
+   export interface Table {
+      lineText?: Array<string>
+      className?: string
+   }
 }
 
 const separateLinesAndTable = (description: string) =>
@@ -52,21 +80,21 @@ const splitLines = (description: string) => {
    return splitted
 }
 
-const convertLine = (line: rowsLines) => {
+const convertLine = (line: rowsLines | string) => {
    const lineText = typeof line === 'string' ? line : line.lineText
    const regStart =
          '<(?:highlight_[1-4]|pve|pvp|bold|link|title|stasis|arc|solar|void|primary|special|heavy|background|center)',
       regEnd = '/>'
    const splittedLine = lineText.split(new RegExp(`(${regStart}.*?${regEnd})`, 'g'))
 
-   const newLine = splittedLine.reduce((acc: any, curr: string) => {
-      // TODO: ==============   fix any
+   const newLine = splittedLine.reduce((acc: Complete.LineText[], curr: string) => {
       const isTableText = curr.startsWith('|')
       if (curr.startsWith('<') || isTableText) {
          const link = curr.match(/(https:.+? )|((?<=https:.+? ).*(?=\/>))/g),
             title = curr.match(/\[.*\]/g)
-         const formula = curr.match(/(ready|stow|range|reload)_\d+/g)?.[0],
-            formulaText = formula ? curr.replace(formula, '').replace(/<formula |\/>/g, '') : undefined
+         const isFormula = curr.includes('<formula'),
+            formula = isFormula ? curr.match(/(ready|stow|range|reload)_\d+/g)?.[0] : '',
+            formulaText = isFormula ? curr.replace(formula || '', '').replace(/<formula |\/>/g, '') : undefined
 
          let className = curr.match(new RegExp(regStart, 'g'))?.join(' ').replaceAll('<', ''),
             cleanText = curr.replace(new RegExp(`${regStart}|${regEnd}`, 'g'), '')
@@ -84,7 +112,7 @@ const convertLine = (line: rowsLines) => {
             title: title ? title[0].replace(/[[\]]/g, '') : undefined,
 
             className: className,
-            text: link ? undefined : title ? cleanText.replace(/\[.*\]/g, '') : formula ? undefined : cleanText
+            text: link ? undefined : title ? cleanText.replace(/\[.*\]/g, '') : isFormula ? undefined : cleanText
          })
          return acc
       }
@@ -96,11 +124,12 @@ const convertLine = (line: rowsLines) => {
 
    // text in table should be 1 array but can be more if where are links or highlights
    // this will compress to single array
-   const checkTableText = (text: any) => {
-      if (text.length == 1) return text
-      return text.reduce((acc: any, text: any) => {
-         Object.entries(text).forEach(([key, value]) => {
-            if (value) acc[key] = `${acc[key] || ''} ${value}`
+   const checkTableText = (textArr: Complete.LineText[]) => {
+      if (textArr.length == 1) return textArr
+      return textArr.reduce((acc, text: Complete.LineText) => {
+         Object.entries(text).forEach(([key, value]: [string, string]) => {
+            type Key = keyof typeof text
+            if (value) acc[key as Key] = `${acc[key as Key] || ''} ${value}`
          })
          return acc
       }, {})
@@ -112,25 +141,25 @@ const convertLine = (line: rowsLines) => {
 
 export default function convertDescription(description: string) {
    const tableAndLines = separateLinesAndTable(description)
-   const splittedLines = tableAndLines.reduce((acc: Array<splittedDescription>, curr) => {
+   const splittedLines = tableAndLines.reduce((acc: LineTextString.Description[], curr: string) => {
       if (curr.startsWith('< table >')) {
-         acc.push(splitLines(curr) as any) // todo: fix any
+         acc.push(splitLines(curr) as LineTextString.Table)
          return acc
       } else {
-         acc.push(...(splitLines(curr) as any)) // todo: fix any
+         acc.push(...splitLines(curr) as rowsLines[])
          return acc
       }
    }, [])
 
-   const completeDescription = splittedLines.map((line) => {
+   const completeDescription = splittedLines.map((line: any): Complete.Description => {
       if (line.table) {
-         const table = line.table.map((row: any) => {
-            const converted = row.lineText?.map((column: any) => convertLine(column))
+         const table = line.table.map((row: LineTextString.Table) => {
+            const converted = row.lineText?.flatMap((column: string) => {console.log(column); return convertLine(column)})
             return { ...row, lineText: converted }
          })
-         return { ...line, table: table }
+         return { ...line, table: table as Complete.Table }
       } else {
-         return convertLine(line as any) // TODO fix this its not suppose to be as any
+         return convertLine(line) as Complete.LineText
       }
    })
    return completeDescription

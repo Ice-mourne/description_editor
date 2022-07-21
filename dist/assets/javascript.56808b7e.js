79ff006c5d758e11ee913fa629a7d3a71027d7c6
Icemourne <Cboweris@gmail.com>
2022-07-21 21:53:52 +0300

    fix

--- dist/assets/javascript.56808b7e.js
+++ dist/assets/javascript.56808b7e.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript.bd71f85d.js";import"./index.4e8f743f.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript.8ad78d77.js";import"./index.bc75c4b3.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
--- dist/assets/tsMode.3d7e4e33.js
+++ dist/assets/tsMode.3d7e4e33.js
@@ -1,4 +1,4 @@
-import{t as O,m as I}from"./index.4e8f743f.js";/*!-----------------------------------------------------------------------------
+import{t as O,m as I}from"./index.bc75c4b3.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
  * Released under the MIT license
